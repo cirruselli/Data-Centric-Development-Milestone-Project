944688690aa7de8b995809024574e2e9046ffbb3
cirruselli <cirruselli@gmail.com>
2020-11-06 10:23:41 +0000

    Create register.html

--- app.py
+++ app.py
@@ -62,6 +62,11 @@ def logIn():
     return render_template("logIn.html")
 
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
